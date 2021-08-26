0d9105b4dcff4f1ecb4f64086cfbebe71611c16b
Rhealyn Anne Bautista <rhealynanne2@gmail.com>
2021-08-26 17:31:54 +0800

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -21,7 +21,7 @@ const forecast = (latitude, longitude, callback) => {
           body.current.temperature +
           " degrees out. It feels like " +
           body.current.feelslike +
-          " degrees out. "
+          " degrees out. The humidity is " + body.current.humidity + "%."
       );
     }
   });
